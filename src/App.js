f2db7bf47c12bc75cce22bc4e5ee5db7b5d9626b
willmondal <wbmondal@aol.com>
2020-12-09 15:52:29 -0800

    nasa Data imported

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+
+const [data, setData] = useState([])
+
+useEffect(() => {
+
+  axios
+.get('https://api.nasa.gov/planetary/apod?api_key=5ZvwTBmFiSuDDQCU6srtFE12FuSh0Rm6lgFihzQT')
+.then(res => {
+  setData(res.data)
+})
+.catch(err=>{
+  console.log('err')
+})
+
+}, []);
+
   return (
     <div className="App">
       <p>
